d61af3cf981ddbb54ffd2e78ce0c32a9cf6c7fdf
lucas <lucas.cplacido@hotmail.com>
2023-02-12 19:18:35 -0300

    criacao do modelo.py e aplicacao flask

---
+++ flask/app.py
@@ -0,0 +1,39 @@
+from modelo import recomendation
+from flask import Flask, request, render_template
+import random
+
+app = Flask(__name__)
+
+# Lista de IDs de usuários
+model = recomendation()
+user_ids = model.ratings.rating.values
+
+# Lista de IDs de filmes
+movie_ids = model.movies.movieId.values # -->  mapping de id de filme ?
+
+# Função que retorna um ID de filme aleatório
+def get_random_movie_id():
+    return random.choice(movie_ids)
+
+@app.route("/")
+def index():
+    # Obtém o ID de usuário enviado na requisição
+    user_id = request.args.get("user_id")
+
+    # Verifica se o ID de usuário foi enviado na requisição
+    if user_id is None:
+        return render_template("index.html")
+
+    # Converte o ID de usuário para inteiro
+    user_id = int(user_id)
+
+    # Verifica se o ID de usuário está na lista de IDs de usuários
+    if user_id not in user_ids:
+        return "Usuário inválido", 400
+
+    recomendacao = model.recomendation(user_id)
+
+    return render_template("result.html", user_id=user_id, movie_name=recomendacao)
+
+if __name__ == "__main__":
+    app.run()
---
+++ flask/modelo.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import numpy as np
+import json
+
+caminho_movies = "../data/movies.csv"
+caminho_ratings = "../data/ratings.csv"
+
+class recomendation():
+    def __init__(self, caminho_movies = caminho_movies, caminho_ratings = caminho_ratings):
+        self.movies = pd.read_csv(caminho_movies)
+        self.ratings = pd.read_csv(caminho_ratings).drop("timestamp", axis = 1)
+
+    staticmethod
+    def distance(x, y):
+        return np.linalg.norm(x - y)
+
+    def rating_user(self, id):
+        return self.ratings.loc[self.ratings.userId == id][["movieId", "rating"]].set_index("movieId")
+
+    def distance_users(self, id1, id2, minimo = 5): 
+        user1 = self.rating_user(id1)
+        user2 = self.rating_user(id2)
+        users = user1.join(user2, how='inner', lsuffix='_user1', rsuffix='_user2')
+        if len(users) < minimo:
+            return None
+        info = recomendation.distance(users.rating_user1, users.rating_user2)
+        return (id1, id2, info)
+
+    def one_vs_all(self, id, subset = None):
+        user_rating = self.ratings.userId.unique()
+        if subset:
+            user_rating = user_rating[:subset]
+        lista = [self.distance_users(id, user) for user in user_rating]
+        lista = list(filter(None, lista))
+        df = pd.DataFrame(lista, columns=["one", "all", "distance"])
+        return df
+
+    def closer(self, id1, subset = None, k_closest = 10):
+        info = self.one_vs_all(id1, subset)
+        info = info.sort_values(by="distance", ascending=True)
+        info = info.set_index("all").drop(id1, axis = 0)
+        return info.head(k_closest)
+
+    def recomendation(self, id1, subset = None, k_closest = 10): # KNN
+
+        closest = self.closer(id1, subset = subset, k_closest = k_closest)
+        closest_users = closest.index 
+        closest_rating = self.ratings.set_index("userId").loc[closest_users]
+
+        recommends = closest_rating.groupby("movieId").mean()[["rating"]]
+        recommends = recommends.sort_values("rating", ascending=False)
+        recommends = recommends.join(self.movies)
+        final = recommends.head(10)
+        final = {id:titulo for id, titulo in enumerate(final.title)}
+        return json.dumps(final)
+
+# if __name__ == "__main__":
+#     recomendation()
